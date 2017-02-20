8c5989cbd6e787a09067b28ad4e30a3174b3e878
aiya000 <aiya000.develop@gmail.com>
2017-02-21 04:31:06 +0900

    Modify EliLiteral for dynamic typing

--- src/Elin/Type.hs
+++ src/Elin/Type.hs
@@ -14,9 +14,12 @@ import Data.Text (Text)
 -- this type doesn't mean the file of source code
 type SourceCode = Text
 
-data EliAST = forall a. Show a => EliLit (EliLiteral a)  -- ^ dynamic typing
-instance Show EliAST where
-  show (EliLit x) = show x
-
-newtype EliLiteral a = EliLiteral { unEliLiteral :: a }
+data EliAST = EliLit EliLiteral
   deriving (Show)
+
+
+--NOTE: Can I use newtype instead of data ?
+-- | Dynamic typed value
+data EliLiteral = forall a. Show a => EliLiteral a
+instance Show EliLiteral where
+  show (EliLiteral x) = show x
