8818092fde883ee0262778971780109d92f0d10a
aiya000 <aiya000.develop@gmail.com>
2017-02-21 04:32:06 +0900

    Add data type for about variable

--- src/Elin/Type.hs
+++ src/Elin/Type.hs
@@ -7,6 +7,7 @@ module Elin.Type
   , EliLiteral (..)
   ) where
 
+import Data.Map.Lazy (Map)
 import Data.Text (Text)
 
 -- |
@@ -23,3 +24,9 @@ data EliAST = EliLit EliLiteral
 data EliLiteral = forall a. Show a => EliLiteral a
 instance Show EliLiteral where
   show (EliLiteral x) = show x
+
+-- | The identifier for function, variable and etc
+type EliIdentifier = String
+
+-- | Dynamic scope (global)
+type EliScope = Map EliIdentifier EliLiteral
