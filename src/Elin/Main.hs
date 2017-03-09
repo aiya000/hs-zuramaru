abd198adc5b1f245e80926fcbfe16aaa3f88b947
aiya000 <aiya000.develop@gmail.com>
2017-03-09 12:49:29 +0900

    Improve defaultMain for exception of head

--- src/Elin/Main.hs
+++ src/Elin/Main.hs
@@ -1,16 +1,23 @@
 module Elin.Main (defaultMain) where
 
-import Control.Exception.Safe (try, SomeException)
 import Control.Monad ((<$!>))
+import Safe (headMay)
 import System.Environment (getArgs)
 import qualified Data.Text as T
 import qualified Elin.Parser as EP
 
 
+-- |
+-- Parse specified source code.
+-- And evaluate its result
 defaultMain :: IO ()
 defaultMain = do
-  --FIXME: try didn't catch the error
-  eitherFilePath <- try (head <$> getArgs)
-  case eitherFilePath of
-    Left  e -> putStrLn $ "error: " ++ show (e :: SomeException)
-    Right a -> T.pack <$!> readFile a >>= EP.parseTest
+  maybeFilePath <- headMay <$> getArgs
+  case maybeFilePath of
+    Nothing -> putStrLn description
+    Just x  -> T.pack <$!> readFile x >>= EP.parseTest
+
+
+-- TODO
+description :: String
+description = "TODO (description)"
