2de6dbd117747f457474dbfa44ace5ef6bac78e4
aiya000 <aiya000.develop@gmail.com>
2017-07-15 16:27:03 +0900

    Add a test for step 2

--- test/Steps/Step2Test.hs
+++ test/Steps/Step2Test.hs
@@ -6,7 +6,7 @@ import Data.Text (Text)
 import Maru.Type (SExpr(..), SExprLike(..))
 import System.IO.Silently (silence)
 import Test.Tasty (TestTree)
-import Test.Tasty.HUnit (testCase, (@?=), Assertion)
+import Test.Tasty.HUnit (testCase, (@?=), Assertion, assertFailure)
 import qualified Maru.Eval as Eval
 
 
@@ -32,10 +32,21 @@ test_evaluator_evaluates =
                                 (Cons (AtomInt 3)
                                 Nil)))
           !?= AtomInt 6
+      , testCase "(1 2) (naked expression) to an error" $ do
+          result <- evalInitSilently $ Cons (AtomInt 1) (Cons (AtomInt 2) Nil)
+          case result of
+            Left e -> return () -- The naked expression should be the error
+            Right a -> assertFailure $ "A right result is detected, but a naked expression should be an error: " ++ show a
       ]
+
+
+evalInitSilently :: SExpr -> IO (Either SomeException (SExpr, MaruEnv))
+evalInitSilently = silence . Eval.eval Eval.initialEnv
 
 
 (!?=) :: SExpr -> SExpr -> Assertion
 origin !?= expected = do
-  (actual, _) <- silence $ Eval.eval Eval.initialEnv origin
-  actual @?= expected
+  result <- evalInitSilently origin
+  case result of
+    Left e -> assertFailure $ "An error is caught: " ++ show e
+    Right (actual, _) -> actual @?= expected
