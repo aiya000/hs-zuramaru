6d6718a86fe58f76a0f55732e9d457133d44c3ad
aiya000 <aiya000.develop@gmail.com>
2017-07-29 16:13:14 +0900

    Create Maru.Type.Lens module

--- src/Maru/Type.hs
+++ src/Maru/Type.hs
@@ -2,11 +2,13 @@
 module Maru.Type
   ( module Maru.Type.Eff
   , module Maru.Type.Eval
+  , module Maru.Type.Lens
   , module Maru.Type.Parser
   , module Maru.Type.SExpr
   ) where
 
 import Maru.Type.Eff
 import Maru.Type.Eval
+import Maru.Type.Lens
 import Maru.Type.Parser
 import Maru.Type.SExpr
