3d06df127d6b20fa123fa489fccab0194f42e55c
aiya000 <aiya000.develop@gmail.com>
2017-09-29 22:34:44 +0900

    Add dirty dirty dirty test to Step4Test, oh my god
    これを見ているdoctestに知見のあるHaskeller、助けてくれ
    
    Maru.Type.SExprという名前空間にあるreadableという関数に対して
    テストをかけたんだ。
    
    そうしたらGHC姉さんに
    「SExpr型がMaru.Type.SExprと、zuramaru-0.1.0.0:Maru.Type.SExprの
    　両方に定義されているわ」
    と言われてしまった。
    
    しかしそもそもこのプロジェクト名がzuramaru、
    現在のバージョンが0.1.0.0だ。
    
    全く意味がわからない。
    
    stack exec doctest src/Maru/Type/SExpr.hs
    を実行すると、同じ結果が確認できると思う。
    
    私のOSはArchLinuxだ。
    
    これ以下は実際の、件のエラー内容になる。
    どうか助けて欲しい。
    
            test/Tasty.hs
              doctest
                src/Maru/Type/SExpr.hs:
            ### Failure in src/Maru/Type/SExpr.hs:153: expression `readable <$> parse "10"'
            expected: Right "10"
             but got:
                      <interactive>:87:14: error:
                          • Couldn't match type ‘zuramaru-0.1.0.0:Maru.Type.SExpr.SExpr’
                                           with ‘SExpr’
                            NB: ‘SExpr’ is defined at src/Maru/Type/SExpr.hs:(58,1)-(62,21)
                                ‘zuramaru-0.1.0.0:Maru.Type.SExpr.SExpr’
                                  is defined in ‘Maru.Type.SExpr’ in package ‘zuramaru-0.1.0.0’
                            Expected type: Either ParseErrorResult SExpr
                              Actual type: ParseResult
                          • In the second argument of ‘(<$>)’, namely ‘parse "10"’
                            In the expression: readable <$> parse "10"
                            In an equation for ‘it’: it = readable <$> parse "10"
            Examples: 27  Tried: 23  Errors: 0  Failures: 1

--- test/Steps/Step4Test.hs
+++ test/Steps/Step4Test.hs
@@ -2,7 +2,8 @@
 
 module Steps.Step4Test where
 
-import Maru.Type (readable)
+import Maru.Parser (parse)
+import Maru.Type (readable, SExpr(..))
 import MaruTest (runCodeInstantly)
 import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (testCase, (@?=))
@@ -17,4 +18,29 @@ test_boolean_literals =
       let expected = "false"
       (sexpr, _, _) <- runCodeInstantly expected
       readable sexpr @?= expected
+  ] ++ readable_func_endomorphism_test
+
+
+-- FIXME: I write below doctests as `readable`'s document comment, but the fcking error is happened. why? I write this test because is is happened. Please fix this, oh my god.
+-- >>> readable <$> parse "10"
+-- Right "10"
+-- >>> parse . readable $ AtomInt 10
+-- Right (AtomInt 10)
+--
+-- >>> readable <$> parse "(+ 1 2)"
+-- Right "(+ 1 2)"
+-- >>> let result = parse . readable $ Cons (AtomSymbol "+") (Cons (AtomInt 1) (Cons (AtomInt 2) Nil))
+-- >>> result == Right (Cons (AtomSymbol "+") (Cons (AtomInt 1) (Cons (AtomInt 2) Nil)))
+-- True
+readable_func_endomorphism_test :: [TestTree]
+readable_func_endomorphism_test =
+  [ testCase "`readable` and `parse` are endomorphic if the format is ignored" $ do
+      (readable <$> parse "10")
+        @?= Right "10"
+      parse (readable $ AtomInt 10)
+        @?= Right (AtomInt 10)
+      readable <$> parse "(+ 1 2)"
+        @?= Right "(+ 1 2)"
+      parse (readable $ Cons (AtomSymbol "+") (Cons (AtomInt 1) (Cons (AtomInt 2) Nil)))
+        @?= Right (Cons (AtomSymbol "+") (Cons (AtomInt 1) (Cons (AtomInt 2) Nil)))
   ]
