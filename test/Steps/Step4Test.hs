aa3082c3d47afc779d665dfa91cbed5353eb28e8
aiya000 <aiya000.develop@gmail.com>
2017-10-01 19:18:40 +0900

    Add test cases to test_boolean_literals

--- test/Steps/Step4Test.hs
+++ test/Steps/Step4Test.hs
@@ -2,19 +2,24 @@
 
 module Steps.Step4Test where
 
-import Maru.Type (readable, SExpr(..))
+import Data.Text (Text)
+import Maru.Type (readable)
 import MaruTest (runCodeInstantly)
 import Test.Tasty (TestTree)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.HUnit (testCase, (@?=), Assertion)
 
 test_boolean_literals :: [TestTree]
 test_boolean_literals =
   [ testCase "`true` literal is printable" $ do
-      let expected = "true"
-      (sexpr, _, _) <- runCodeInstantly expected
-      readable sexpr @?= expected
+      itShouldBePrintable "true" "true"
+      itShouldBePrintable "(def! x true)" "true"
   , testCase "`false` literal is printable" $ do
-      let expected = "false"
-      (sexpr, _, _) <- runCodeInstantly expected
-      readable sexpr @?= expected
+      itShouldBePrintable "false" "false"
+      itShouldBePrintable "(def! x false)" "false"
   ]
+  where
+    -- 'printable' can be shown as 'printed'
+    itShouldBePrintable :: Text -> Text -> Assertion
+    itShouldBePrintable printable printed = do
+      (sexpr, _, _) <- runCodeInstantly printable
+      readable sexpr @?= printed
