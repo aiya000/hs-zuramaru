4b0bb0cfb1c56967708acd0ad763cf0967d54efc
aiya000 <aiya000.develop@gmail.com>
2017-10-03 22:00:15 +0900

    Add tests about negative number literals and do
    In my opinion, we should start to use Data.Semigroup.<>
        instead of Data.Monoid.<>
    because if we use Semigroup,
        Data.Semigroup.<> keeps more consistency

--- test/Steps/Step4Test.hs
+++ test/Steps/Step4Test.hs
@@ -2,11 +2,21 @@
 
 module Steps.Step4Test where
 
+import Data.Semigroup ((<>))
 import Data.Text (Text)
-import Maru.Type (readable)
-import MaruTest (runCodeInstantly)
+import Maru.Type (readable, MaruSymbol(..), MaruEnv, SExpr(..))
+import MaruTest (runCodeInstantly, runCode)
 import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (testCase, (@?=), Assertion)
+import qualified Data.Text as T
+
+
+-- | 'printable' can be shown as 'printed'
+itShouldBePrintable :: Text -> Text -> Assertion
+itShouldBePrintable printable printed = do
+  (sexpr, _, _) <- runCodeInstantly printable
+  readable sexpr @?= printed
+
 
 test_boolean_literals :: [TestTree]
 test_boolean_literals =
@@ -17,9 +27,38 @@ test_boolean_literals =
       itShouldBePrintable "false" "false"
       itShouldBePrintable "(def! x false)" "false"
   ]
+
+
+-- | My additional test
+test_integral_negative_literals :: [TestTree]
+test_integral_negative_literals =
+  [ testCase "can be evaluated" $ do
+      itShouldBePrintable "-1" "-1"
+      itShouldBePrintable "(+ -1 2)" "1"
+      itShouldBePrintable "(+ 2 -1)" "1"
+  ]
+
+
+test_do_macro :: [TestTree]
+test_do_macro =
+  [ testCase "evaluates taken arguments" $ do
+      (sexpr, env, _) <- runCodeInstantly $ "(do (def! x 10)" <>
+                                            "    (def! y (+ x 1))" <>
+                                            "    (def! z (+ y 1)))"
+      sexpr @?= AtomInt 12
+      "x" `existedIn` env
+      "y" `existedIn` env
+      "z" `existedIn` env
+  ]
   where
-    -- 'printable' can be shown as 'printed'
-    itShouldBePrintable :: Text -> Text -> Assertion
-    itShouldBePrintable printable printed = do
-      (sexpr, _, _) <- runCodeInstantly printable
-      readable sexpr @?= printed
+    -- She lives in the world
+    existedIn :: MaruSymbol -> MaruEnv -> Assertion
+    she `existedIn` world = do
+      -- She undressed her dress.
+      -- Her bareness was so mysterious...
+      let nakedGoddess = unMaruSymbol she
+      -- Also, her child was birthed.
+      runCode world nakedGoddess
+      -- She, her child, and all the families are so happy !
+      return ()
+      --NOTE: Don't afraid to delete these comment if these are obstacle :P
