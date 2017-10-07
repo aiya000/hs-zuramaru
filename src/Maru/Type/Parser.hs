3e0032d0c2eb3f08a441ed8f4072144c1ffe82a7
aiya000 <aiya000.develop@gmail.com>
2017-10-07 15:51:06 +0900

    Remove unused items of Maru.Type.Parser

--- src/Maru/Type/Parser.hs
+++ src/Maru/Type/Parser.hs
@@ -5,84 +5,57 @@
 -- | The type for Maru.Parser
 module Maru.Type.Parser
   ( ParseLog (..)
-  , MaruState (..)
-  , parseLogs
-  , parseNestLevel
+  , ParseLogs
   , MaruParser
   , runMaruParser
-  , tell
-  , tellMsg
-  , increaseNestLevel
-  , decreaseNestLevel
+  , tell'
   , ParseErrorResult
   ) where
 
 import Control.Applicative (Alternative)
-import Control.Lens ((%=), (+=), (-=), makeLenses)
 import Control.Monad (MonadPlus)
 import Control.Monad.Fail (MonadFail)
-import Control.Monad.State.Class (MonadState, gets)
+import Control.Monad.State.Class (MonadState, modify)
 import Control.Monad.State.Lazy (State, runState)
-import Data.Monoid ((<>), Monoid)
 import Data.Sequence (Seq, (|>))
 import Data.String (IsString)
 import Data.Text (Text)
 import Maru.Type.SExpr
 import Text.Megaparsec (ParsecT, ParseError, Dec, runParserT)
 import Text.Megaparsec.Prim (MonadParsec)
-import qualified Data.Text as T
 import qualified GHC.Exts as L
 
+-- | The parse result of the failure
 type ParseErrorResult = ParseError MaruToken Dec
 
 -- | A log message of the parsing, for debug
 newtype ParseLog = ParseLog { unParseLog :: Text }
   deriving (IsString, Monoid)
 
--- | Current state of `MaruParser`
-data MaruState = MaruState
-  { _parseLogs      :: Seq ParseLog
-  , _parseNestLevel :: Int
-  }
-makeLenses ''MaruState
+type ParseLogs = Seq ParseLog
 
--- | A parser for the code of maru
-newtype MaruParser a = MaruParser { unMaruParser :: ParsecT Dec Text (State MaruState) a }
+-- |
+-- A parser for the code of maru
+--
+-- NOTE:
+-- This `MonadState` instance is instead of `MonadWriter`,
+-- because `ParsecT` is not the `MonadWriter` instance.
+newtype MaruParser a = MaruParser { unMaruParser :: ParsecT Dec Text (State ParseLogs) a }
   deriving ( Functor, Applicative, Monad
            , Alternative, MonadPlus
-           , MonadState MaruState
+           , MonadState ParseLogs
            , MonadParsec Dec Text
            , MonadFail
            )
 
 -- |
 -- Run parser and extract result and logs.
---
--- But `MaruState`'s `_parseLogs` is converted to the list.
-runMaruParser :: MaruParser a -> Text -> (Either (ParseError MaruToken Dec) a, [ParseLog])
+runMaruParser :: MaruParser a -> Text -> (Either ParseErrorResult a, [ParseLog])
 runMaruParser parser source =
-  let initialState = MaruState [] 0
-      bareness     = unMaruParser parser
-      (result, MaruState logs _) = flip runState initialState $ runParserT bareness "" source
+  let bareness = unMaruParser parser
+      (result, logs) = flip runState [] $ runParserT bareness "" source
   in (result, L.toList logs)
 
 -- | Append a log to head of _parseLogs in the parsing
-tell :: ParseLog -> MaruParser ()
-tell log = parseLogs %= (|> log)
-
--- |
--- Apply text to `tell`.
--- The parseNestLevel is appended as indent to text automatically
-tellMsg :: Text -> MaruParser ()
-tellMsg txt = do
-  indentLevel <- gets _parseNestLevel
-  let indent = ParseLog . T.pack $ replicate indentLevel '\t'
-  tell $ indent <> ParseLog txt
-
--- | Add 1 to parseNestLevel of state
-increaseNestLevel :: MaruParser ()
-increaseNestLevel = parseNestLevel += 1
-
--- | Subtract 1 to parseNestLevel of state
-decreaseNestLevel :: MaruParser ()
-decreaseNestLevel = parseNestLevel -= 1
+tell' :: ParseLog -> MaruParser ()
+tell' log = modify (|> log)
