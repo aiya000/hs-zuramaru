5196f7e395009079a84642ab84120896ac78fbba
aiya000 <aiya000.develop@gmail.com>
2017-10-27 20:08:16 +0900

    Improve DocTest
    - Don't stop the continuation of doctest ('main') even if the exception is detected
    - Consume a TODO

--- test/doctest/DocTest.hs
+++ test/doctest/DocTest.hs
@@ -1,17 +1,30 @@
+import Control.Exception.Safe (catch, SomeException)
+import Control.Monad (when)
+import Data.Monoid (All(..))
+import Data.Profunctor (dimap)
+import System.Exit (exitFailure)
 import Test.DocTest (doctest)
 
---TODO: These test may return the correctly return code constantly. Return the failure result if it is failed
 
 main :: IO ()
 main = do
-  doctestIt "src/Maru/Type/SExpr.hs" ["src/Maru/Parser.hs"]
-  doctestIt "src/Maru/Type/Eval.hs" []
-  doctestIt "src/Maru/Eval.hs" []
-  doctestIt "src/Maru/Eval/RuntimeOperation.hs" []
+  xs <- sequence [ doctestIt "src/Maru/Type/SExpr.hs" ["src/Maru/Parser.hs"]
+                 , doctestIt "src/Maru/Type/Eval.hs" []
+                 , doctestIt "src/Maru/Eval.hs" []
+                 , doctestIt "src/Maru/Eval/RuntimeOperation.hs" []
+                 ]
+  when (dimap (map All) getAll mconcat xs) exitFailure
 
 
-doctestIt :: FilePath -> [FilePath] -> IO ()
+doctestIt :: FilePath -> [FilePath] -> IO Bool
 doctestIt path dependencies = do
   putStrLn $ path ++ ":"
-  doctest $ dependencies ++ [path]
+  x <- isExceptionThrown . doctest $ dependencies ++ [path]
   putStrLn ""
+  return x
+  where
+    isExceptionThrown :: IO a -> IO Bool
+    isExceptionThrown x = (x >> return False) `catch'` (return . const True)
+
+    catch' :: IO Bool -> (SomeException -> IO Bool) -> IO Bool
+    catch' = catch
