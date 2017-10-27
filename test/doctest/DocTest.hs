b6c4bee41790d3230986314c40e861423393d2bc
aiya000 <aiya000.develop@gmail.com>
2017-10-27 23:44:06 +0900

    いみわかんねーよばーかばーか！！！！！
    ……でも好き、いつもありがとう。

--- test/doctest/DocTest.hs
+++ test/doctest/DocTest.hs
@@ -1,30 +1,17 @@
-import Control.Exception.Safe (catch, SomeException)
-import Control.Monad (when)
-import Data.Monoid (All(..))
-import Data.Profunctor (dimap)
-import System.Exit (exitFailure)
 import Test.DocTest (doctest)
 
 
 main :: IO ()
 main = do
-  xs <- sequence [ doctestIt "src/Maru/Type/SExpr.hs" ["src/Maru/Parser.hs"]
-                 , doctestIt "src/Maru/Type/Eval.hs" []
-                 , doctestIt "src/Maru/Eval.hs" []
-                 , doctestIt "src/Maru/Eval/RuntimeOperation.hs" []
-                 ]
-  when (dimap (map All) getAll mconcat xs) exitFailure
+  doctestIt "src" "src/Maru/Type/SExpr.hs"
+    [ "src/Maru/Parser.hs"
+    , "src/Maru/Preprocessor.hs"
+    , "src/Maru/Eval.hs"
+    , "src/Maru/Type/Eval.hs"
+    , "src/Maru/Eval/RuntimeOperation.hs"
+    ]
 
-
-doctestIt :: FilePath -> [FilePath] -> IO Bool
-doctestIt path dependencies = do
-  putStrLn $ path ++ ":"
-  x <- isExceptionThrown . doctest $ dependencies ++ [path]
-  putStrLn ""
-  return x
-  where
-    isExceptionThrown :: IO a -> IO Bool
-    isExceptionThrown x = (x >> return False) `catch'` (return . const True)
-
-    catch' :: IO Bool -> (SomeException -> IO Bool) -> IO Bool
-    catch' = catch
+doctestIt :: FilePath -> FilePath -> [FilePath] -> IO ()
+doctestIt baseDir path dependencies = do
+  let nico = "-i" ++ baseDir
+  doctest $ (nico:dependencies) ++ [path]
