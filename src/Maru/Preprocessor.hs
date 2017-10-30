fff26c84474fe2660fcccb9bd6deec4c23f9f353
aiya000 <aiya000.develop@gmail.com>
2017-10-30 20:01:41 +0900

    Implement the preprocessor

--- src/Maru/Preprocessor.hs
+++ src/Maru/Preprocessor.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | The preprocessor between 'CallowSExpr' and 'SExpr'
 module Maru.Preprocessor
   ( preprocess
   ) where
 
-import Maru.Type (CallowSExpr(..), SExpr(..))
+import Maru.Type
 
--- | Process a 'SExpr' to the desired form before 'Maru.Eval.eval'
+-- |
+-- Process a 'SExpr' to the desired form before 'Maru.Eval.eval'
+--
+-- Regard the symbol of "quote" to a quote macro
+--
+-- >>> preprocess $ Cons' (AtomSymbol' "quote") (Cons' (AtomInt 10) Nil')
+-- Quote (AtomInt 10)
 preprocess :: CallowSExpr -> SExpr
-preprocess = undefined
+preprocess (Cons' (AtomSymbol' "quote") (Cons' x Nil')) = Quote $ growUp x
+preprocess x = growUp x
