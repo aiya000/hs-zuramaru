20ee45030543a343df5bb96038e842052aad222d
aiya000 <aiya000.develop@gmail.com>
2018-01-10 20:36:10 +0900

    Rename Step2Test.hs to FuncTest.hs

--- test/tasty/Maru/Eval/FuncTest.hs
+++ test/tasty/Maru/Eval/FuncTest.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Steps.Step2Test where
+module Maru.Eval.FuncTest where
 
 import Control.Exception.Safe (SomeException)
 import Maru.Type (SExpr(..), MaruEnv, SimplificationSteps)
@@ -9,32 +9,21 @@ import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (testCase, (@?=), Assertion, assertFailure)
 import qualified Maru.Eval as Eval
 
-
--- | See https://github.com/kanaka/mal/blob/master/process/guide.md#step-2-eval
-test_evaluator_calculates :: [TestTree]
-test_evaluator_calculates =
+test_evaluator_evaluates_pure_function :: [TestTree]
+test_evaluator_evaluates_pure_function =
   [ testCase "(+ 2 3) to 5" $
       Cons (AtomSymbol "+") (Cons (AtomInt 2) (Cons (AtomInt 3) Nil))
       !?= AtomInt 5
   , testCase "(+ 2 (* 3 4)) to 14" $
       Cons (AtomSymbol "+") (Cons (AtomInt 2) (Cons (Cons (AtomSymbol "*") (Cons (AtomInt 3) (Cons (AtomInt 4) Nil))) Nil))
       !?= AtomInt 14
-  ] ++ myAddtionalCases
-  where
-    myAddtionalCases :: [TestTree]
-    myAddtionalCases =
-      [ testCase "(+ 1 2 3) to 6" $
-          Cons (AtomSymbol "+") (Cons (AtomInt 1)
-                                (Cons (AtomInt 2)
-                                (Cons (AtomInt 3)
-                                Nil)))
-          !?= AtomInt 6
-      ]
-
-
-evalInitSilently :: SExpr -> IO (Either SomeException (SExpr, MaruEnv, SimplificationSteps))
-evalInitSilently = silence . Eval.eval Eval.initialEnv
-
+  , testCase "(+ 1 2 3) to 6" $
+      Cons (AtomSymbol "+") (Cons (AtomInt 1)
+                            (Cons (AtomInt 2)
+                            (Cons (AtomInt 3)
+                            Nil)))
+      !?= AtomInt 6
+  ]
 
 (!?=) :: SExpr -> SExpr -> Assertion
 origin !?= expected = do
@@ -42,3 +31,6 @@ origin !?= expected = do
   case result of
     Left e -> assertFailure $ "An error is caught: " ++ show e
     Right (actual, _, _) -> actual @?= expected
+  where
+    evalInitSilently :: SExpr -> IO (Either SomeException (SExpr, MaruEnv, SimplificationSteps))
+    evalInitSilently = silence . Eval.eval Eval.initialEnv
