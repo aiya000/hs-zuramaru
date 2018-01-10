8da599ef6243ae59ccbd2259a478c3e9b6ec850f
aiya000 <aiya000.develop@gmail.com>
2018-01-10 21:17:26 +0900

    Add the specs to this macro

--- test/tasty/Maru/Eval/MacroTest.hs
+++ test/tasty/Maru/Eval/MacroTest.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedLists #-}
+ LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Maru.Eval.MacroTest where
@@ -168,4 +168,13 @@ test_quote_macro =
       "'(1 2 3)" `shouldBeEvaluatedTo` "(1 2 3)"
       "''2" `shouldBeEvaluatedTo` "(quote 2)"
       "'(10 '20)" `shouldBeEvaluatedTo` "(10 (quote 20))"
+  ]
+
+
+test_this_macro :: [TestTree]
+test_this_macro =
+  [ testCase "cab be called as a current function recursively" $
+      "((fn (x) (if (<= 0 x) 0 (+ x (this (- x 1))))) 5)" `shouldBeEvaluatedTo` "15"
+  , testCase "means a mostly inner if it nests" $
+      "((fn (a) ((fn (x) (if (<= 0 x) 0 (this (- x 1)))) (- a 1))) 5)" `shouldBeEvaluatedTo` "10"
   ]
